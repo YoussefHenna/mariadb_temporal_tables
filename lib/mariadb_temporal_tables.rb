aa201450602c5778d973995ce98a508f58a3820c
Youssef Henna <youssef.hisham14@gmail.com>
2022-12-29 14:58:53 +0100

    Updated system_versioning_set_author to be visible without referencing module

--- lib/mariadb_temporal_tables.rb
+++ lib/mariadb_temporal_tables.rb
@@ -3,13 +3,11 @@ module MariaDBTemporalTables
   require "mariadb_temporal_tables/concerns/system_versioning"
   require "mariadb_temporal_tables/concerns/application_versioning"
   require "mariadb_temporal_tables/concerns/combined_versioning"
+end
 
-
-  # Sets the author to be referenced in new versions created on models with system versioning enabled
-  #
-  # @param author [Object] Object of author to be referenced when new version is created. Must have 'id' to be referenced.
-  def system_versioning_set_author(author)
-    Thread.current[:mariadb_temporal_tables_current_author] = author
-  end
-
+# Sets the author to be referenced in new versions created on models with system versioning enabled
+#
+# @param author [Object] Object of author to be referenced when new version is created. Must have 'id' to be referenced.
+def system_versioning_set_author(author)
+  Thread.current[:mariadb_temporal_tables_current_author] = author
 end
